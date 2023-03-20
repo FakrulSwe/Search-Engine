a23cdf87c18dbfa0e08b942eebd0d7ae51850670
Fakrul Islam <dev.rupok02@gmail.com>
2023-03-20 14:14:55 +0600

    It is a google Search Engine

---
+++ Js/bing.js
@@ -0,0 +1,8 @@
+let query = document.querySelector('.query');
+let serchBtn = document.querySelector('.searchBtn');
+
+
+serchBtn.onclick = function() {
+    let url ='https://www.bing.com/search?q='+query.value;
+    window.open(url, '_self');
+}
